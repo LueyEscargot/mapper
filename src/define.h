93d3a295178b27abbc8228f657fd724541f27274
Liu Yu <source@liuyu.com>
2019-10-09 23:57:49 +0800

    apply changes of session buffer(SessionBuffer --> Buffer)

--- src/define.h
+++ src/define.h
@@ -12,7 +12,7 @@
 #ifndef __MAPPER_DEFINE_H__
 #define __MAPPER_DEFINE_H__
 
-#include "sessionBuffer.hpp"
+#include "buffer.hpp"
 
 namespace mapper
 {
@@ -23,9 +23,9 @@ static const int BUFFER_SIZE = 1024;
 
 typedef enum STATE_MACHINE
 {
-    INIT = 0,
+    INITIALIZED = 0,
     CONNECTING,
-    CONNECTED,
+    ESTABLISHED,
     CLOSE,
     FAIL
 } StateMachine_t;
@@ -42,16 +42,22 @@ typedef struct SOCK
 {
     int soc;
     int events;
+    bool fullFlag;
     SockType_t type;
     SESSION *pSession;
+    Buffer<BUFFER_SIZE> buffer;
 
-    void init(SockType_t _type, SESSION *_pSession, int _soc = 0, int _events = 0)
+    void init(SockType_t _type, SESSION *_pSession)
     {
         type = _type;
         pSession = _pSession;
-
+    }
+    void init(int _soc, int _events)
+    {
         soc = _soc;
         events = _events;
+        fullFlag = false;
+        buffer.init();
     }
 } Sock_t;
 
@@ -62,15 +68,17 @@ typedef struct SESSION
     int64_t lastAccessTime;
     StateMachine_t status;
 
-    SessionBuffer<BUFFER_SIZE, BUFFER_SIZE> buffer;
-
+    void init()
+    {
+        clientSoc.init(SOCK_TYPE::CLIENT_SOCK, this);
+        hostSoc.init(SOCK_TYPE::HOST_SOCK, this);
+    }
     void init(int _clientSoc, int _clientEvents, int _hostSoc = 0, int _hostEvents = 0)
     {
-        clientSoc.init(SOCK_TYPE::CLIENT_SOCK, this, _clientSoc, _clientEvents);
-        hostSoc.init(SOCK_TYPE::HOST_SOCK, this, _hostSoc, _hostEvents);
+        clientSoc.init(_clientSoc, _clientEvents);
+        hostSoc.init(_hostSoc, _hostEvents);
         int64_t lastAccessTime = 0;
-        StateMachine_t status = STATE_MACHINE::INIT;
-        buffer.init();
+        StateMachine_t status = STATE_MACHINE::INITIALIZED;
     }
 } Session_t;
 
