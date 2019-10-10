1ccaa1bde8dce76d3e3afad2eb9e778732d1ff42
Liu Yu <source@liuyu.com>
2019-10-10 22:55:40 +0800

    create new structure MAP_DATA and refactory struct SOCK

--- src/define.h
+++ src/define.h
@@ -12,11 +12,15 @@
 #ifndef __MAPPER_DEFINE_H__
 #define __MAPPER_DEFINE_H__
 
+#include <assert.h>
+#include <stdio.h>
+#include <string>
 #include "buffer.hpp"
 
 namespace mapper
 {
 
+static const int MAX_HOST_NAME = 253;
 static const int MAX_SESSIONS = 10240;
 static const int SESSION_TIMEOUT = 90;
 static const int BUFFER_SIZE = 1024;
@@ -38,19 +42,42 @@ typedef enum SOCK_TYPE
 
 struct SESSION;
 
+typedef struct MAP_DATA
+{
+    int port;
+    char target[MAX_HOST_NAME + 1];
+    int targetPort;
+
+    MAP_DATA(int _port, std::string _target, int _targetPort) : MAP_DATA(_port, _target.c_str(), _targetPort) {}
+    MAP_DATA(int _port, const char *_target, int _targetPort)
+    {
+        assert(0 <= _port && _port <= 65535 && _target && strlen(_target) && 0 <= _targetPort && _targetPort <= 65535);
+        port = _port, snprintf(target, MAX_HOST_NAME + 1, "%s", _target), targetPort = _targetPort;
+    }
+} MapData_t;
+
 typedef struct SOCK
 {
     int soc;
     int events;
     bool fullFlag;
+    SOCK *pareSock;
     SockType_t type;
     SESSION *pSession;
     Buffer<BUFFER_SIZE> buffer;
 
-    void init(SockType_t _type, SESSION *_pSession)
+    void init(SOCK_TYPE _type, SESSION *_pSession)
     {
         type = _type;
         pSession = _pSession;
+        if (type == SOCK_TYPE::CLIENT_SOCK)
+        {
+            pareSock = &pSession->hostSoc;
+        }
+        else
+        {
+            pareSock = &pSession->clientSoc;
+        }
     }
     void init(int _soc, int _events)
     {
