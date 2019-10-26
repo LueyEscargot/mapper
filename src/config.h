aa63d686e1be20d3a468dae7200e2ec9fd067a97
Liu Yu <source@liuyu.com>
2019-10-27 03:11:14 +0800

    create and using mapper::Config

--- src/config.h
+++ src/config.h
@@ -12,18 +12,46 @@
 #define __MAPPER_CONFIG_H__
 
 #include <exception>
+#include <map>
+#include <regex>
 #include <string>
+#include <vector>
+#include "define.h"
 
 namespace mapper
 {
 
 class Config
 {
-public:
+protected:
     static const char *GLOBAL_SECTION;
 
-    bool parse(const char *cfgUri);
-    std::string get(const char *_key, const char *_section = nullptr);
+    static std::regex REG_FOR_TRIM_LEADING_SPACE;
+    static std::regex REG_FOR_TRIM_TAIL_SPACE;
+    static std::regex REG_FOR_TRIM_COMMENTS;
+    static std::regex REG_SECTION;
+    static std::regex REG_CONFIG;
+    static std::regex REG_MAPPING;
+    static std::regex REG_VALID_IPV4;
+
+    using SECTION = std::map<std::string, std::string>;
+    using CONFIG = std::map<std::string, SECTION>;
+    using TARGET = std::pair<std::string, int>;
+
+public:
+    Config();
+
+    bool parse(const char *file);
+    std::string get(std::string key, std::string section = "", std::string defaultValue = "");
+    std::vector<MapData_t> &getMapData();
+
+protected:
+    void parseLine(std::string &line);
+
+    std::string mCurSection;
+    CONFIG mConfig;
+    std::map<int, TARGET> mRawMapData;
+    std::vector<MapData_t> mMapDatas;
 };
 
 } // namespace mapper
--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,21 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <exception>
 #include <vector>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
-#include "mapper.h"
+#include "config.h"
 #include "define.h"
+#include "mapper.h"
 
-static const char *CONFIG_FILE = "config.json";
+static const char *CONFIG_FILE = "config.ini";
 static const char *LOG_FILE = "mapper.log";
 static const auto LOG_LEVEL = spdlog::level::debug;
 
 void init();
+mapper::Config gConf;
 mapper::Mapper gMapper;
 
 int main(int argc, char *argv[])
@@ -30,17 +33,9 @@ int main(int argc, char *argv[])
 
     spdlog::info("[main] Start");
 
-    spdlog::debug("[main] load map data");
-    std::vector<mapper::MapData_t> mapDatas;
-    mapper::MapData_t mapData1(6911, "192.168.1.66", 6911);
-    mapper::MapData_t mapData2(8118, "192.168.1.66", 8118);
-
-    mapDatas.push_back(mapData1);
-    mapDatas.push_back(mapData2);
-
     // run mapper
     spdlog::debug("[main] run mapper");
-    if (!gMapper.run(mapper::MAX_SESSIONS, &mapDatas))
+    if (!gMapper.run(mapper::MAX_SESSIONS, &gConf.getMapData()))
     {
         spdlog::error("[main] run mapper fail");
         std::exit(EXIT_FAILURE);
@@ -53,11 +48,30 @@ int main(int argc, char *argv[])
 
 void init()
 {
-    // init logger
-    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(LOG_FILE, true);
-    spdlog::set_default_logger(
-        std::make_shared<spdlog::logger>(
-            "mapper", spdlog::sinks_init_list({console_sink, file_sink})));
-    spdlog::set_level(LOG_LEVEL);
+    try
+    {
+        if (!gConf.parse(CONFIG_FILE))
+        {
+            printf("[init] load config file[%s] fail", CONFIG_FILE);
+            std::exit(EXIT_FAILURE);
+        }
+
+        // init logger
+        {
+            auto level = spdlog::level::from_str(gConf.get("level", "log", "info"));
+
+            auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+            auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(LOG_FILE, true);
+            spdlog::set_default_logger(
+                std::make_shared<spdlog::logger>(
+                    "mapper", spdlog::sinks_init_list({console_sink, file_sink})));
+            spdlog::set_level(level);
+            spdlog::flush_on(level);
+        }
+    }
+    catch (std::exception &e)
+    {
+        printf("[init] catch an exception: %s", e.what());
+        std::exit(EXIT_FAILURE);
+    }
 }
