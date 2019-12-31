42a7137244962d2829b1f53ef06f894768bfeaa7
Liu Yu <source@liuyu.com>
2019-12-31 23:42:28 +0800

    add new tool class: mapper::link::Utils

---
+++ src/link/utils.cpp
@@ -0,0 +1,175 @@
+#include "utils.h"
+#include <assert.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <spdlog/spdlog.h>
+
+using namespace std;
+
+namespace mapper
+{
+namespace link
+{
+
+bool Utils::getIntfAddr(const char *intf, sockaddr_in &sa)
+{
+    if (strcasecmp(intf, "any") == 0)
+    {
+        memset(&sa, 0, sizeof(sa));
+
+        sa.sin_family = AF_INET;
+        sa.sin_addr.s_addr = htonl(INADDR_ANY);
+        return true;
+    }
+
+    struct ifaddrs *ifaddr;
+    if (getifaddrs(&ifaddr))
+    {
+        return false;
+    }
+
+    bool result = [&ifaddr, &intf, &sa]() -> bool {
+        for (auto ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next)
+        {
+            if (ifa->ifa_addr == nullptr ||
+                ifa->ifa_addr->sa_family != AF_INET ||
+                strcasecmp(ifa->ifa_name, intf))
+            {
+                continue;
+            }
+
+            memcpy(&sa, ifa->ifa_addr, sizeof(sockaddr_in));
+
+            char ip[INET_ADDRSTRLEN];
+            if (inet_ntop(AF_INET, &sa, ip, INET_ADDRSTRLEN))
+            {
+                spdlog::debug("[Utils::getIntfAddr] IPv4 address of interface[{}]: {}", intf, ip);
+                return true;
+            }
+            else
+            {
+
+                spdlog::error("[Utils::getIntfAddr] get IPv4 address of interface[{}] fail", intf);
+                return false;
+            }
+        }
+
+        spdlog::error("[Utils::getIntfAddr] IPv4 address of interface[{}] not found", intf);
+        return false;
+    }();
+
+    freeifaddrs(ifaddr);
+
+    return result;
+}
+
+bool Utils::getAddrInfo(const char *host,
+                        const char *service,
+                        const Protocol_t protocol,
+                        struct addrinfo **ppAddrInfo)
+{
+    addrinfo hints;
+
+    // init hints
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_flags = 0;
+    hints.ai_canonname = nullptr;
+    hints.ai_addr = nullptr;
+    hints.ai_next = nullptr;
+    if (protocol == Protocol_t::TCP)
+    {
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+    }
+    else
+    {
+        hints.ai_socktype = SOCK_DGRAM;
+        hints.ai_protocol = IPPROTO_UDP;
+    }
+
+    int nRet = getaddrinfo(host, service, &hints, ppAddrInfo);
+    if (nRet != 0)
+    {
+        spdlog::error("[Utils::getAddrInfo] getaddrinfo fail: {}", gai_strerror(nRet));
+        return false;
+    }
+
+    return true;
+}
+
+void Utils::closeAddrInfo(struct addrinfo *pAddrInfo)
+{
+    freeaddrinfo(pAddrInfo);
+}
+
+int Utils::createServiceSoc(Protocol_t protocol, sockaddr_in *sa, socklen_t salen)
+{
+    // create socket
+    int soc = createSoc(protocol, true);
+    if (soc <= 0)
+    {
+        spdlog::error("[Utils::createServiceSoc] create socket fail. {} - {}", errno, strerror(errno));
+        return -1;
+    }
+    if ([&protocol, &sa, &salen, &soc]() -> bool {
+            // set reuse
+            int opt = 1;
+            if (setsockopt(soc, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+            {
+                spdlog::error("[Utils::createServiceSoc] set reuse fail. {} - {}", errno, strerror(errno));
+                return false;
+            }
+            // bind
+            if (bind(soc, (struct sockaddr *)sa, salen))
+            {
+                spdlog::error("[Utils::createServiceSoc] bind fail. {} - {}", errno, strerror(errno));
+                return false;
+            }
+            // listen
+            if (protocol == Protocol_t::TCP && listen(soc, SOMAXCONN << 1))
+            {
+                spdlog::error("[Utils::createServiceSoc] listen fail. {} - {}", errno, strerror(errno));
+                return false;
+            }
+            return true;
+        }())
+    {
+        return soc;
+    }
+    else
+    {
+        close(soc);
+        return -1;
+    }
+}
+
+int Utils::createSoc(Protocol_t protocol, bool nonblock)
+{
+    // create socket
+    int soc = socket(AF_INET,
+                     protocol == Protocol_t::TCP ? SOCK_STREAM : SOCK_DGRAM,
+                     0);
+    if (soc <= 0)
+    {
+        spdlog::error("[Utils::createServiceSoc] create socket fail. {} - {}", errno, strerror(errno));
+        return -1;
+    }
+
+    // set to non-block
+    if (nonblock)
+    {
+        int flags = fcntl(soc, F_GETFL);
+        if (flags < 0 || fcntl(soc, F_SETFL, flags | O_NONBLOCK) < 0)
+        {
+            spdlog::error("[Utils::createServiceSoc] set to non-block fail. {} - {}", errno, strerror(errno));
+            close(soc);
+            return -1;
+        }
+    }
+
+    return soc;
+}
+
+} // namespace link
+} // namespace mapper
---
+++ src/link/utils.h
@@ -0,0 +1,39 @@
+/**
+ * @file utils.h
+ * @author Liu Yu (source@liuyu.com)
+ * @brief Utils Class.
+ * @version 1
+ * @date 2019-12-30
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#ifndef __MAPPER_LINK_UTILS_H__
+#define __MAPPER_LINK_UTILS_H__
+
+#include <sys/socket.h>
+#include <string>
+#include "type.h"
+
+namespace mapper
+{
+namespace link
+{
+
+class Utils
+{
+public:
+    static bool getIntfAddr(const char *intf, sockaddr_in &sa);
+    static bool getAddrInfo(const char *host,
+                            const char *service,
+                            const Protocol_t protocol,
+                            struct addrinfo **ppAddrInfo);
+    static void closeAddrInfo(struct addrinfo *pAddrInfo);
+    static int createSoc(Protocol_t protocol, bool nonblock);
+    static int createServiceSoc(Protocol_t protocol, sockaddr_in *sa, socklen_t salen);
+};
+
+} // namespace link
+} // namespace mapper
+
+#endif // __MAPPER_LINK_UTILS_H__
